97bfec9a1ed36e36ebbaa28cfb9a537e549873b1
Surya Kumar <suryakumar531rsk@gmail.com>
2025-07-28 13:45:10 +0530

    Added footer

--- src/Landingpage.jsx
+++ src/Landingpage.jsx
@@ -6,6 +6,7 @@ import { Joblisting } from './Components-LandingPage/Joblisting'
 import { Topcompanies } from './Components-LandingPage/Topcompanies'
 import { Choice } from './Components-LandingPage/Choice'
 import { Newsletter } from './Components-LandingPage/Newsletter'
+import { Footer } from './Components-LandingPage/Footer'
 
 export const Landingpage = () => {
   return (
@@ -17,49 +18,7 @@ export const Landingpage = () => {
       <Topcompanies />
       <Choice />
       <Newsletter />
-
-      <footer>
-        <div>
-          <div>
-            <h3>ProJobs</h3>
-            <p>Your trusted partner in career advancement.</p>
-            <div>
-              <a href="#"><svg fill="currentColor" viewBox="0 0 24 24"></svg></a>
-              <a href="#"><svg fill="currentColor" viewBox="0 0 24 24"></svg></a>
-              <a href="#"><svg fill="currentColor" viewBox="0 0 24 24"></svg></a>
-            </div>
-          </div>
-          <div>
-            <h3>Quick Links</h3>
-            <ul>
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Contact Us</a></li>
-              <li><a href="#">FAQs</a></li>
-              <li><a href="#">Blog</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3>Job Seekers</h3>
-            <ul>
-              <li><a href="#">Create Profile</a></li>
-              <li><a href="#">Browse Jobs</a></li>
-              <li><a href="#">Saved Jobs</a></li>
-              <li><a href="#">Application Status</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3>Employers</h3>
-            <ul>
-              <li><a href="#">Post a Job</a></li>
-              <li><a href="#">Find Talent</a></li>
-              <li><a href="#">Employer Dashboard</a></li>
-            </ul>
-          </div>
-        </div>
-        <div>
-          &copy; {new Date().getFullYear()} ProJobs. All rights reserved.
-        </div>
-      </footer>
+    <Footer />
     </>
   )
 }
