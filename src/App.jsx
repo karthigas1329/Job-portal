f0450198921ceae69dcb5933be24ad754557ae43
karthigas1329 <karthigamowni@gmail.com>
2025-11-18 15:54:05 +0530

    job
    landing, jobseeker, login

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { Elogin } from './Components-EmployerSignup/Elogin'
 import { Jlogin } from './Components-JobseekerSignup/Jlogin'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Jsignup } from './Components-JobseekerSignup/Jsignup'
-import { Jcreatepassword } from './Components-JobseekerSignup/Jcreatepassword'
 import { Jforgotpassword } from './Components-JobseekerSignup/Jforgotpassword'
 import { Afterloginlanding } from './Components-Jobseeker/Afterloginlanding'
 
@@ -29,10 +28,7 @@ const router = createBrowserRouter([{
   path: '/Job-portal/jobseeker/signup',
   element: <Jsignup />,
 },
-{
-  path: '/Job-portal/jobseeker/login/forgotpassword/createpassword',
-  element: <Jcreatepassword />,
-},
+
 {
   path: '/Job-portal/jobseeker/',
   element: <Afterloginlanding />,
